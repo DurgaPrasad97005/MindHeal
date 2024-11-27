4306e8abf83627b49d8472df7540e72f805dda20
Durga Prasad <durgaprasad97005@gmail.com>
2024-11-26 23:05:35 -1200

    Update AboutHero design

--- src/components/AboutHero.jsx
+++ src/components/AboutHero.jsx
@@ -1,21 +1,20 @@
 import React from "react";
-import "./AboutHero.css"
+import "./AboutHero.css";
 
 export default function AboutHero() {
     return (
         <div className="about-container">
             <div className="about-hero">
-                <div className="about-col">
-                    <div className="about-row">
-                        <h1>Welcome to MindHeal</h1>
-                        <h2>Your mental wellness journey starts here.</h2>
-                    </div>
-                    <div className="about-row">
-                        <h2>"Empowering individuals to prioritize mental wellness."</h2>
-                    </div>
+                <div className="about-col-1">
+                    <h2>Your mental wellness</h2>
+                    <h1>MIND</h1>
+                    <h2>Empowering individuals to </h2>
                 </div>
-                <div className="about-col">
-                    image
+
+                <div className="about-col-2">
+                    <h2>journey starts here</h2>
+                    <h1>HEAL</h1>
+                    <h2>prioritize mental wellness</h2>
                 </div>
             </div>
         </div>
